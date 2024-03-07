0c1e62ce6b1be7c26b64af3f9eb628a1e0578c94
tuyizereerneste <tuyizereernest@yahoo.com>
2024-03-07 18:41:02 +0200

    Currency class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,7 +1,7 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
+    this.code = code;
+    this.name = name;
   }
 
   get newCode() {
@@ -27,6 +27,6 @@ export default class Currency {
   }
 
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+    return `${this.name} (${this.code})`;
   }
 }
