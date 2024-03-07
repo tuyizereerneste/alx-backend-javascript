1668653aa2c8549a08387e1af99212cd817ccdc6
tuyizereerneste <tuyizereernest@yahoo.com>
2024-03-07 23:28:34 +0200

    default string

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,26 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(nameValue) {
+    this._name = nameValue;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(codeValue) {
+    this._code = codeValue;
+  }
+
+  toString() {
+    return this._code;
+  }
+}
