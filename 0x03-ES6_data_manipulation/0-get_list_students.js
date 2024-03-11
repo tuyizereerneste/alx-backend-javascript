f48e23f1a46d07e89e978f32e0ffc817dfdb71be
tuyizereerneste <tuyizereernest@yahoo.com>
2024-03-11 19:02:49 +0200

    list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
 export default function getListStudents() {
-  const obj = [
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstname: 'James', location: 'Columbia' },
     { id: 5, firstname: 'Serena', location: 'San Francisco' },
   ];
-  return obj;
 }
